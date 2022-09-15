22b24f6266197e8a7cb0cf161b2a777543d43e7b
Andy <andy@andeh.uk>
2022-09-15 14:41:16 +0100

    initial commit

---
+++ src/main/java/com/bisecthosting/mcordlink/MCordLink.java
@@ -0,0 +1,53 @@
+package com.bisecthosting.mcordlink;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class MCordLink extends JavaPlugin implements Listener {
+
+    static int generate_code() {
+        int code = (int) Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
+        // Do some kinda check here to see if the code already exists for a different player, if it does then generate
+        // another.
+        // Jake can you do the thing where we store codes alongside mc usernames + discord user IDs pls
+        return code;
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        Logger logger = this.getLogger();
+        logger.log(Level.INFO, "Plugin has loaded.");
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+        Logger logger = this.getLogger();
+        logger.log(Level.INFO, "Plugin has loaded.");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        int code = generate_code();
+        player.sendMessage(
+                ChatColor.YELLOW + "Your code is " + ChatColor.GREEN + ChatColor.UNDERLINE
+                        + code + ChatColor.RESET + ChatColor.YELLOW + ". Please send this code to " +
+                        ChatColor.RED + "BisectStudios Verification#1234!"
+
+        );
+    }
+}
