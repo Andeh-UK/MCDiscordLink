3475d9d1f96efe794693dd5755e471d2c6d4b0ad
Andy <andy@andeh.uk>
2022-09-16 11:52:49 +0100

    MySQL Database Connection Setup

--- src/main/java/com/bisecthosting/mcordlink/discord/MessageListener.java
+++ src/main/java/com/bisecthosting/mcordlink/discord/MessageListener.java
@@ -2,25 +2,37 @@ package com.bisecthosting.mcordlink.discord;
 
 import com.bisecthosting.mcordlink.MCordLink;
 
+import com.bisecthosting.mcordlink.yaml.YamlCreation;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class MessageListener  extends ListenerAdapter{
+public class MessageListener extends ListenerAdapter {
 
     private MCordLink plugin;
+    private YamlCreation yamlCreation;
+    private String channelID;
     private JDA jda;
     private char unicodeWarn = '\u26A0';
     private char unicodeYes = '\u2714';
-    public MessageListener(MCordLink main) {
+    public MessageListener(MCordLink main, YamlCreation yaml) {
         this.plugin = main;
+        this.yamlCreation = yaml;
     }
 
     public void init() {
-        this.plugin.getDiscordLauncher().getJDA();
+        this.channelID = this.yamlCreation.getChannelID();
+        this.jda = this.plugin.getDiscordLauncher().getJDA();
         this.jda.addEventListener(this);
+    }
+
+    public TextChannel getChannel() {
+        if(this.channelID == "") {
+            return null;
+        }
+        return this.jda.getTextChannelById(this.channelID);
     }
 
     @Override
@@ -28,10 +40,16 @@ public class MessageListener  extends ListenerAdapter{
         String msg = event.getMessage().getContentRaw();
         User user = event.getAuthor();
 
-        if (event.getChannel() instanceof PrivateChannel) {
-            if (user.isBot() || user.isSystem()) {
-                return;
-            }
+        if(user.isBot() || user.isSystem()) {
+            return;
+        }
+
+        if(this.getChannel() == null) {
+            return;
+        }
+
+        if(event.getChannel().equals(this.getChannel())) {
+            // Do check to see if code exists, if it does remove the code and give the user a discord role
         }
     }
 
--- src/main/java/com/bisecthosting/mcordlink/yaml/YamlCreation.java
+++ src/main/java/com/bisecthosting/mcordlink/yaml/YamlCreation.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
+
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -15,7 +16,7 @@ public class YamlCreation {
     private File file;
     public YamlCreation(MCordLink main) {
         this.plugin = main;
-        this.file = new File(this.plugin.getDataFolder(), "configuration.yml");
+        this.file = new File(this.plugin.getDataFolder(), "config.yml");
     }
 
     public void init() {
@@ -23,12 +24,40 @@ public class YamlCreation {
             this.plugin.getDataFolder().mkdir();
         }
 
-        if (!this.file.exists()) {
+        if(!this.file.exists()) {
             try {
                 this.file.createNewFile();
+
+                // make default config go here
             } catch (IOException e) {
-                this.plugin.getLogger().info(ChatColor.RED + "" + ChatColor.BOLD + "[ERROR]" + ChatColor.WHITE + "Unable to c");
+                this.plugin.getLogger().info(ChatColor.RED + "" + ChatColor.BOLD + "[ERROR]" + ChatColor.WHITE
+                        + "Unable to create config file.");
             }
         }
+    }
+
+    public String getChannelID() {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(this.file);
+        return config.getString("Channel ID");
+    }
+
+    private YamlConfiguration makeDefaultConfiguration() {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(this.file);
+
+        FileConfiguration configFile = YamlConfiguration.loadConfiguration(this.file);
+        configFile.setComments("MCL", Arrays.asList("Welcome to the MCordLink Configuration.", "", "",
+        "Made by BH | Hiraku#2623. For any issues of any bugs, please contact him on discord @ " +
+                "https://discord.com/users/1012482462586241134", "To begin with, enter your " +
+                "Discord Text Channel ID below.", "", "", "For example: \"Channel ID\": \"983464907204882435\""));
+
+        try {
+            configFile.save(this.file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        config.set("Channel ID", "");
+
+        return config;
     }
 }
