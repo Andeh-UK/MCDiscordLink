799ea847587c700b8e6f0ce7eb8a0140c21b9914
Andy <andy@andeh.uk>
2022-09-16 18:30:39 +0100

    Join Listener almost complete, need to add user to db

--- src/main/java/com/bisecthosting/mcordlink/listeners/JoinListener.java
+++ src/main/java/com/bisecthosting/mcordlink/listeners/JoinListener.java
@@ -1,5 +1,9 @@
 package com.bisecthosting.mcordlink.listeners;
 
+import com.bisecthosting.mcordlink.MCordLink;
+import com.bisecthosting.mcordlink.database.DBConnection;
+
+import com.bisecthosting.mcordlink.yaml.YamlCreation;
 import com.marcusslover.plus.lib.text.Text;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -7,27 +11,56 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.sql.Connection;
+import java.util.Map;
+
 public class JoinListener implements Listener {
 
-    static int generate_code() {
+    private DBConnection dbConnection = null;
+
+    public JoinListener(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+    static String generate_code() {
         int code = (int) Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
+        String final_code = Integer.toString(code);
         // Do some kinda check here to see if the code already exists for a different player, if it does then generate
         // another.
         // Jake can you do the thing where we store codes alongside mc usernames + discord user IDs pls
-        return code;
+        return final_code;
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        int code = generate_code();
+    private void send_code(Player player, String code) {
         player.sendMessage(
                 ChatColor.YELLOW + "Your code is " + ChatColor.GREEN + ChatColor.UNDERLINE
                         + code + ChatColor.RESET + ChatColor.YELLOW + ". Please send this code to " +
                         ChatColor.RED + "BisectStudios Verification#1234!"
 
         );
-        Text.of("&7 Coc").send(player);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String name = player.getName();
+        Map<String, String> player_data = this.dbConnection.getPlayer(name);
+        String code = player_data.get("code");
+        String discord_id = player_data.get("discord_id");
+        if (code == null) {
+            String new_code = generate_code();
+            this.dbConnection.addPlayer(new_code, name);
+            this.send_code(player, new_code);
+        } else {
+            if (discord_id == null) {
+                this.send_code(player, code);
+            } else {
+                if(name.equals("ExHiraku")) {
+                    player.sendMessage("Fuck you");
+                }
+                player.sendMessage("Welcome Back, " + name + "!");
+            }
+        }
     }
 
 }
